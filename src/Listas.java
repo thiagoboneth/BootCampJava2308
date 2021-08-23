5b17e67934fc083dc5e19fd6096a5000d1254522
thiagoboneth <thiago.luis@mercadolivre.com>
2021-08-23 14:45:18 -0300

    Estudo de Lista

---
+++ src/Listas.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listas {
+    public static void main(String[] args) {
+        List<String> languagens = new ArrayList<>();
+        languagens.add("Java");
+        languagens.add("Python");
+        languagens.add("JS");
+        languagens.add("Go");
+
+        //System.out.println(languagens.size());
+        //for (String language:languagens){
+        //System.out.println(language);
+        //System.out.println(languagens.indexOf("Python"));
+        //System.out.println(languagens.get(2));
+        //languagens.remove(2);
+        //System.out.println(languagens);
+        
+        }
+}
