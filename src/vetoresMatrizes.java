344ed4e67230875a028a55ae01cdb92273bca911
thiagoboneth <thiago.luis@mercadolivre.com>
2021-08-23 12:14:24 -0300

    Exercicio Array e Matriz

---
+++ src/vetoresMatrizes.java
@@ -0,0 +1,12 @@
+public class vetoresMatrizes {
+    public static void main(String[] args) {
+        String[] cidades = {"Londres","Madrid","Nova York","Buenos Aires","Asuncion","Sao Paulo","Lima","Santiado de Chile","Lisboa","Tokyo"};
+        double[][] temperaturas = {{-2,33}, {-3,32}, {-8,27}, {4,37}, {6,42}, {5,43}, {0,39}, {-7,26}, {-1,31}, {-10,35}};
+        for (int i = 0; i < cidades.length; i++){
+            System.out.println(cidades[i]+" Temperatura minima de "+temperaturas[i][0] + " Temperatura maxima "+ temperaturas[i][1]);
+            //for (int u = 0; u < 2; u++){
+            //    System.out.println(temperaturas[i][u]);
+            }
+        }
+    }
+//}
